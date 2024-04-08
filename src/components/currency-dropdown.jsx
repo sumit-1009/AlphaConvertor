77529c6082eca094b1be451bbad4bb0389588406
Sumit Sahu <sssahu1009@gmail.com>
2024-04-08 11:37:57 +0530

    Final

--- src/components/currency-dropdown.jsx
+++ src/components/currency-dropdown.jsx
@@ -2,44 +2,58 @@ import React from 'react'
 import { HiOutlineStar, HiStar } from 'react-icons/hi'
 
 const CurrencyDropdown = ({
-    currencies,
-    currency,
-    setCurrency,
-    favorites,
-    handleFavorite,
-    title = "",
+  currencies,
+  currency,
+  setCurrency,
+  favorites,
+  handleFavorite,
+  title = "",
 }) => {
+  const isFavorite = (curr) => favorites.includes(curr);
 
-
-  const isFavorites = curr => favorites.includes(curr);
-    
   return (
-    <div className='mt-1 relative'>
-      <label  className="block text-sm font-medium text-gray-700" htmlFor={title}> {title} </label>
-      <div>
-        <select value={currency} onChange={(e) => {setCurrency(e.target.value)}} className="w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1">
+    <div>
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {title}
+      </label>
+
+      <div className="mt-1 relative">
+        <select
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
           {favorites.map((currency) => {
-            return <option className='bg-gray-200' value={currency} key={currency}> 
-                   {currency}  
+            return (
+              <option className="bg-gray-200" value={currency} key={currency}>
+                {currency}
               </option>
+            );
           })}
           <hr />
           {currencies
-           .filter((c) => !favorites.includes(c))
-           .map((currency) => {
-            return (
-              <option value={currency} key={currency}> 
-                   {currency}  
-              </option> 
-            )
-          })}
+            .filter((c) => !favorites.includes(c))
+            .map((currency) => {
+              return (
+                <option value={currency} key={currency}>
+                  {currency}
+                </option>
+              );
+            })}
         </select>
-        <button onClick={handleFavorite(currency)} className='absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5'>
-          {isFavorites(currency) ? <HiStar /> : <HiOutlineStar />}
+
+        <button
+          onClick={() => handleFavorite(currency)}
+          className="absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5"
+        >
+          {isFavorite(currency) ? <HiStar /> : <HiOutlineStar />}
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CurrencyDropdown
